616373caf428a8176bcc63dc4b7f2e96ff213da9
pashmirk <pashmirk@gmail.com>
2019-01-13 17:44:39 +0000

    Added app.mjs and start.mjs to start server

---
+++ app.mjs
@@ -0,0 +1,23 @@
+import createHttpError from 'http-errors';
+import express from 'express';
+import morgan from 'morgan';
+import errorHandler from './utils/error-handler.mjs';
+import { router as indexRouter } from './routes/v1/index.mjs';
+import dotenv from 'dotenv';
+
+export const app = express();
+
+dotenv.config();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header('Content-Type', 'application/json');
+  next();
+});
+app.use('/', indexRouter);
+app.use((req, res, next) => {
+  next(createHttpError(404));
+});
+app.use(errorHandler);
---
+++ start.mjs
@@ -0,0 +1,8 @@
+import { app } from './app.mjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
